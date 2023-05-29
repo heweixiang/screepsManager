727b57b6cd74c51667c1731afbc5b98c5543563d
touchfish <touchfish.cc@outlook.com>
2023-05-29 17:12:21 +0800

    1

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,23 @@
 // 第三方模块
-import bodyParser from 'body-parser';
-import express from 'express';
-import { NextFunction, Request, Response } from 'express'; // express 申明文件定义的类型
+import bodyParser from "body-parser";
+import express from "express";
+import { NextFunction, Request, Response } from "express"; // express 申明文件定义的类型
+import schedule from "node-schedule";
+// 引入ScheduledTask
+import ScheduledTask from "./utils/scheduledTask";
 
 // 自定义模块
-import { systemConfig } from './config';
+import { systemConfig } from "./config";
 
 const app = express();
 
 // 处理 post 请求的请求体，限制大小最多为 20 兆
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
-app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+app.use(bodyParser.json({ limit: "20mb" }));
 
-// error handler
-app.use(function(err: Error, req: Request, res: Response, next: NextFunction) {
-  return res.sendStatus(500);
-});
+// 启动定时任务
+ScheduledTask();
 
-app.listen(systemConfig.port, function() {
+app.listen(systemConfig.port, function () {
   console.log(`the server is start at port ${systemConfig.port}`);
 });
---
+++ server/src/utils/scheduledTask.ts
@@ -0,0 +1,7 @@
+// 定时任务
+const init = () => {
+  console.log('定时任务')
+
+}
+
+export default init
