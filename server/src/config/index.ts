43451d0e999cdc5fe334a61998029f220a57b068
Touch Fish <touchfish.cc@outlook.com>
2023-08-02 11:30:42 +0800

    1

--- server/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-import { ISystemConfig } from '@/types/config';
-
-const systemConfig: ISystemConfig = {
-  port: 8001
-};
-
-export { systemConfig };
--- server/src/utils/scheduledTask.ts
+++
@@ -1,7 +0,0 @@
-// 定时任务
-const init = () => {
-  console.log('定时任务')
-
-}
-
-export default init
